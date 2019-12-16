2632d93a47de1122e211d9ccc1d4400edde93260
dw0rdptr <bsms0418@icloud.com>
2019-12-16 06:16:50 +0000

    Update profile crawler
    Get userid/posts from parsed html

--- profile_crawl.py
+++ profile_crawl.py
@@ -4,16 +4,27 @@
 
 from bs4 import BeautifulSoup
 import requests
+import re
 import sys
-
 
 
 base_url = "https://www.instagram.com/"
 user_name = sys.argv[1]
-user_feed = "?__a=1" 	# to get public user feed
+#user_feed = "?__a=1" 	# to get public user feed
 
-url = base_url+user_name + user_feed
-
+url = base_url+user_name
 r = requests.get(url)
 
-print(r.text)
+#Parse Posts
+pattern = r"\d+.Posts"
+result = re.search(pattern, r.text)
+posts = int(re.findall('\d+',result.group())[0])
+print("posts:",posts)
+
+
+#Parse userid
+pattern = r"owner.:\D+\d+"
+result = re.search(pattern, r.text)
+user_id = int(re.findall('\d+',result.group())[0])
+print("userid:",user_id)
+
