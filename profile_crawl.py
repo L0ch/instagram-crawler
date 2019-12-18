9d3bb27777cac1841a2ce1abec4caf2cc5757036
dw0rdptr <bsms0418@icloud.com>
2019-12-18 13:59:26 +0000

    Update profile crawler
    Add usage

--- profile_crawl.py
+++ profile_crawl.py
@@ -1,17 +1,22 @@
 #-*- coding:utf-8 -*-
-# 인자1 : username
+# parameter1 : username
 
 
-from bs4 import BeautifulSoup
 import requests
 import re
 import sys
 
 
-base_url = "https://www.instagram.com/"
-user_name = sys.argv[1]
-#user_feed = "?__a=1" 	# to get public user feed
 
+
+base_url = "https://www.instagram.com/"
+
+try:
+	user_name = sys.argv[1]
+except:
+	print("Usage : python3",sys.argv[0],"[username]")
+	exit(1)
+	
 url = base_url+user_name
 r = requests.get(url)
 
