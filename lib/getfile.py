c7406bc07a24e16c57c7e7b5e00d5e3b02346226
dw0rdptr <bsms0418@icloud.com>
2019-12-22 06:06:04 +0000

    Improve performance of getfile
    Import asyncio/aiohttp for improved performance by asynchronous request
    Rename list variables

--- lib/getfile.py
+++ lib/getfile.py
@@ -3,12 +3,27 @@
 
 import requests
 import re
+import asyncio
+import aiohttp
 
 base_url = "https://www.instagram.com/p/"
 user_feed = "/?__a=1" 	# to get public user feed
 
-def download(urls):
+img_pattern = re.compile(r'"display_url":".+"')
+video_pattern = re.compile(r'"video_url":".+"')	
+
+async def get_media_url(shortcode, img_urls, video_urls):
+	url = base_url + shortcode + user_feed
 	
+	async with aiohttp.ClientSession() as session:
+		async with session.get(url) as res:
+			json = await res.text()
+			json = json.replace(',',',\n')
+			img_urls += img_pattern.findall(json)
+			video_urls += video_pattern.findall(json)
+			
+
+def download(urls):
 	for url in urls:
 		pattern = r'\d+_\d+_\d+.+\?'
 		file_name = re.search(pattern, url).group().replace('?','')
@@ -20,36 +35,27 @@ def download(urls):
 		
 # parameter : shortcode list
 # return : img url list
-def source(shortcode):
-	cnt = 0
-	img_url = []
-	video_url = []
+def source(shortcodes):
+	img_urls = []
+	video_urls = []
 	
-	
-	img_pattern = re.compile(r'"display_url":".+"')
-	video_pattern = re.compile(r'"video_url":".+"')
-	
-	for short in shortcode:
-		url = base_url + short + user_feed
-		r = requests.get(url)
-		json = r.text.replace(',',',\n')
-		img_url += img_pattern.findall(json)
-		
-		video_url += video_pattern.findall(json)
-		print(cnt)
-		cnt+=1
+	# Asynchronous call
+	# Get image/video urls
+	tasks = [get_media_url(shortcode, img_urls, video_urls) for shortcode in shortcodes]
+	loop = asyncio.get_event_loop()
+	loop.run_until_complete(asyncio.wait(tasks))
 	
 	# split
-	for i in range(0,len(img_url)):
-		img_url[i] = img_url[i].split('"')[3]
+	for i in range(0,len(img_urls)):
+		img_urls[i] = img_urls[i].split('"')[3]
 	
-	for i in range(0,len(video_url)):
-		video_url[i] = video_url[i].split('"')[3]
+	for i in range(0,len(video_urls)):
+		video_urls[i] = video_urls[i].split('"')[3]
 	
-	img_url = list(set(img_url))
+	img_urls = list(set(img_urls))
 	
 	
-	return img_url, video_url
+	return img_urls, video_urls
 	
 	
 
