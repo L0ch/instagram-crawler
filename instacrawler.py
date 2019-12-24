2532300ee236c4a38e579387b8d8f10f13ffb6d1
dw0rdptr <bsms0418@icloud.com>
2019-12-24 14:56:06 +0000

    Update main
    Import argparse
    arguments : --target [username] --op [media type]

--- instacrawler.py
+++ instacrawler.py
@@ -3,38 +3,62 @@ from lib import getfile
 import sys
 import os
 import time
+import argparse
 
-#not : 14s 
-
+	
 
 def main():
+	
 	start = time.time()
-	try:
-		user_name = sys.argv[1]
-	except:
-		print("Usage : python3",sys.argv[0],"[username]")
-		return 0
+	
+	target_help = "Instagram user's name"	
+	op_help = "Crawling media format(image or video, default = all)"
+	
+	parser = argparse.ArgumentParser(description="Instagram media crawler by dw0rdptr")
+	parser.add_argument("--target", required=True, help=target_help)
+	
+	parser.add_argument("--op", required=False, default = "all", choices = ["all", "img", 
+"video"], help=op_help)
+	
+	args = parser.parse_args()
+	
+	target = args.target
+	op = args.op
+	
+	print(target)
+	if op=="all":
+		print("all")
+	elif op=="img":
+		print("img")
+	elif op=="video":
+		print("video")
+	
 	
 	
 	user_info = fetch.profile(user_name) 
 	user_id = user_info[0]
 	posts = user_info[1]
 	
+	
+	print("user_id :", user_id)
+	print("Total post :", posts,'\n')
+	
+	time.sleep(2)
+	
+	print("Get post urls...")
 	shortcode = fetch.shortcode(str(user_id), posts)
+	print(len(shortcode), "posts load Complete.\n")
+	time.sleep(2)
 	
-	
+	print("Get media source urls...")
 	img_url, video_url = getfile.source(shortcode)
+	print(len(img_url), "images,", len(video_url), "videos.\n")
+	time.sleep(2)
 	
-	print(len(img_url))
-	print(len(video_url))
-	
-	if sys.argv[2] == "-img":
-		getfile.download(img_url)
-	elif sys.argv[2] == "-video":
-		getfile.download(video_url)
-	else:
-		getfile.download(img_url)
-		getfile.download(video_url)
+	print("downloading images...")
+	getfile.download(img_url)
+	print("\ndownloading videos...")
+	getfile.download(video_url)
 	
 	
 	print("time :", time.time() - start)
