1ce0fd47270189b59558d51a76294b7c839b1a50
dw0rdptr <bsms0418@icloud.com>
2020-02-02 11:33:16 +0000

    Update fetch.py
    Use json module to parse graphql response instead of regular expression

--- lib/fetch.py
+++ lib/fetch.py
@@ -3,46 +3,48 @@
 
 import requests
 import re
+import json
 import sys
 from tqdm import tqdm
 from tqdm import trange
 
 
-def hashtag(tag_name):
-	bas_url = "https://www.instagram.com/explore/tags/"
-	
+#def hashtag(tag_name):
+	#base_url = "https://www.instagram.com/explore/tags/"
+	#country_code = 
+	#return 20
 
 
 def profile(user_name):
 	base_url = "https://www.instagram.com/"
-	country_code = "/?hl=en"
+	get_feed = "?__a=1"
 	
-	url = base_url+user_name+country_code
-	r = requests.get(url)
+	url = base_url+user_name+get_feed
+	response = requests.get(url).text
 
-	#Parse Total post count
-	pattern = r'"edge_owner_to_timeline_media":{"count":\d+,'
-	result = re.search(pattern, r.text)
 	try:
-		post_cnt = int(result.group().split('"count":')[1].replace(',',''))
-		
+		json_source = json.loads(response)
 	except:
 		print("Account does not exist")
 		exit(1)
-
+		
+	json_user = json_source['graphql']['user']
+	
+	# User ID
+	user_id = json_user['id']
+	# Total post count
+	post_cnt = json_user['edge_owner_to_timeline_media']['count']
+	
+	
+	is_private = json_user['is_private']
+	if is_private == True:
+		print("Cannot access private account")
+		exit(1)
+		
 	if post_cnt == 0:
 		print("Post does not exist, nothing to download")
 		exit(1)
-	
-	#Parse userid
-	pattern = r"owner.:\D+\d+"
-	result = re.search(pattern, r.text)
-	try:
-		user_id = int(re.findall('\d+',result.group())[0])
-	except:
-		print("Cannot access private account")
-		exit(1)
-	
+		
 	return user_id, post_cnt
 	
 
@@ -52,20 +54,25 @@ def profile(user_name):
 
 # return : shortcode(list)
 
-def shortcode(user_id, post_cnt):
+def shortcode(user_id, post_cnt, mode_flag):
 	
 	
 	first = 50			#Post count by once request
 	shortcodes = []
 
-	query_hash = 'e769aa130647d2354c40ea6a439bfc08' #fixed
+	query_hash_tag = '90cba7a4c91000cf16207e4f3bee2fa2'
+	query_hash_user = 'e769aa130647d2354c40ea6a439bfc08' #fixed
 	base_url = 'https://www.instagram.com/'
 
 	# Get json from graphql API 
 	# GET param : query_hash, variables{id, first, after}
-	url = base_url+'graphql/query/?query_hash='+query_hash+'&variables={"id":"'+user_id+'",'  
-
-
+	# mode_flag : user = 1, hashtag = 0
+	if(mode_flag):
+		url = base_url+'graphql/query/?query_hash='+query_hash_user+'&variables={"id":"'+user_id+'",'  
+	else:
+		url = base_url+'graphql/query/?query_hash='+query_hash_tag+'&variables={"tag_name":"'+user_id+'",'  
+	
+		
 	# Total post_cnt / Post count by once request
 	req_cnt = int(post_cnt/first) + bool(post_cnt%first)
 
@@ -73,32 +80,27 @@ def shortcode(user_id, post_cnt):
 	end_cursor = ''
 	for i in tqdm(range(0,req_cnt)):
 		req_url = url + '"first":'+str(first)+',"after":"'+end_cursor+'"}'
-		r = requests.get(req_url)
-	
-		#print(req_url)
-		json = r.text.replace(',',',\n')
-	
+		
+
+		response = requests.get(req_url).text
+		json_source = json.loads(response)
+		
+		json_user = json_source['data']['user']
+		json_edge_owner_to_timeline_media = json_user['edge_owner_to_timeline_media']
+		
+		json_edges = json_edge_owner_to_timeline_media['edges']
+		json_page_info = json_edge_owner_to_timeline_media['page_info']
+		
+
+		for nodes in json_edges:
+			shortcodes.append(nodes['node']['shortcode'])
+
 		# Get end_cursor for next requests
-		p = re.compile(r'"end_cursor":".+==')
-		end_cursor = p.search(json)
+		end_cursor = json_page_info['end_cursor']
 	
-		if end_cursor:
-			end_cursor = end_cursor.group().split('":"')[1]
-			
-		# Get each post shortcode
-		p = re.compile(r'"shortcode":".+"')
-		shortcodes += p.findall(json)
 	
 
-
-	# split
-	for j in range(0,len(shortcodes)):
-		shortcodes[j] = shortcodes[j].split('"')[3]
-	
-	print(len(shortcodes))
-	
 	return shortcodes
 
-		
 		
 		
